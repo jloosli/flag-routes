f8a89597797582901a9ac7144cfa753211569a29
Jared Loosli <jloosli@gmail.com>
2019-06-12 14:29:16 -0600

    Added in directional piece for drivers

--- src/app/modules/drivers/driver-tokens/driver-tokens.component.ts
+++ src/app/modules/drivers/driver-tokens/driver-tokens.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {IDriver} from '@flags/interfaces/driver';
-import {Observable} from 'rxjs';
+import {combineLatest, Observable} from 'rxjs';
 import {DriversService} from '@flags/services/drivers.service';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-driver-tokens',
@@ -9,13 +10,34 @@ import {DriversService} from '@flags/services/drivers.service';
   styleUrls: ['./driver-tokens.component.scss'],
 })
 export class DriverTokensComponent implements OnInit {
-  drivers$: Observable<IDriver[]>;
+  drivers$: Observable<(IDriver & { iconUrl: string | { scale: number, path: string, rotation: number } })[]>;
 
   constructor(private driversSvc: DriversService) {
   }
 
   ngOnInit() {
-    this.drivers$ = this.driversSvc.drivers$;
+    const googleMaps$: Observable<any> = new Observable(obs => {
+      const timeout = () => setTimeout(() => {
+        if ('google' in window) {
+          obs.next((window as any).google.maps);
+        } else {
+          timeout();
+        }
+      }, 100);
+      timeout();
+    });
+    this.drivers$ = combineLatest([this.driversSvc.drivers$, googleMaps$]).pipe(
+      map(([drivers, maps]) => drivers.map(driver => {
+        return {
+          ...driver,
+          iconUrl: {
+            path: driver.heading ? maps.SymbolPath.FORWARD_CLOSED_ARROW : maps.SymbolPath.CIRCLE,
+            rotation: driver.heading || 0,
+            scale: 4,
+            strokeColor: 'silver',
+          },
+        };
+      })),
+    );
   }
-
 }
